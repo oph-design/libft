e33fd3da38e33307f86240384c59b65db036a14c
oph-design <115570424+oph-design@users.noreply.github.com>
2022-11-26 11:41:37 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 DATE=$(date +"%Y-%m-%d %T")
-REPO=https://github.com/oph-design/getnextline.git
+REPO=https://github.com/oph-design/libft.git
 git add .
 git commit -m" $(echo $DATE)"
 git push $REPO
